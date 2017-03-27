db33c9e99c4a9d10478e1ef38aceac167041e157
ciec20151104682 <s20151104682@mac21.local>
2017-03-27 11:07:29 +0800

    Initial Commit

---
+++ counters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  counters
+//
+//  Created by s20151104682 on 17/3/27.
+//  Copyright © 2017年 s20151104682. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
